984b9523ac1d43ac62282565b0e7c849cf5fe3a3
Luberri <fandresimahery@gmail.com>
2025-11-21 12:51:02 +0300

    ptn

--- vueJS/src/main.js
+++ vueJS/src/main.js
@@ -10,7 +10,7 @@ import NotesAnnuel from './NotesAnnuel.vue'
 const routes = [
   { path: '/', component: Semestres },
   { name: 'Etudiants', path: '/etudiants/:semestre/:option', component: Etudiants },
-  { name: 'ReleveNotes', path: '/releve/:idEtudiant/:semestre/:option', component: ReleveNotes }
+  { name: 'ReleveNotes', path: '/releve/:idEtudiant/:semestre/:option', component: ReleveNotes },
   { name: 'EtudiantDetail', path: '/etudiant/:id', component: EtudiantDetail },
   { name: 'NotesAnnuel', path: '/notes/annuel/:idEtudiant/:annee/:option', component: NotesAnnuel }
 ]
