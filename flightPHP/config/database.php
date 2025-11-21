f8ad4dc84a3406e5a5b7ee9c3b9471fea6083f6e
vip-only <safidymirantsoar@gmail.com>
2025-11-21 09:59:44 +0300

    suite

--- flightPHP/config/database.php
+++ flightPHP/config/database.php
@@ -1,9 +1,9 @@
 
 <?php
 Flight::register('db', 'PDO', array(
-    'mysql:host=db;port=3306;dbname=gestion_scolarite;charset=utf8',
-    'root',
-    'root'
+    'mysql:host=127.0.0.1;port=3306;dbname=gestion_scolarite;charset=utf8',
+    'root',      // utilisateur MySQL dans ton conteneur
+    'admin'      // mot de passe MySQL
 ), function ($db) {
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 });
---
+++ vueJS/src/Etudiants.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h2>Étudiants du {{ $route.params.semestre }} (Option {{ $route.params.option }})</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Moyenne S1</th>
+            <th>Moyenne S2</th>
+            <th>Moyenne S3</th>
+            <th>Moyenne S4</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="etudiant in etudiants" :key="etudiant.id">
+            <td>{{ etudiant.nom }}</td>
+            <td>{{ etudiant.prenom }}</td>
+            <td>{{ etudiant.meta?.S1 ?? '-' }}</td>
+            <td>{{ etudiant.meta?.S2 ?? '-' }}</td>
+            <td>{{ etudiant.meta?.S3 ?? '-' }}</td>
+            <td>{{ etudiant.meta?.S4 ?? '-' }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
